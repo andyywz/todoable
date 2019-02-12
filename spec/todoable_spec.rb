3d25237dd9e4bc64e51f706e061b16ae78518681
Andy Yong <andyywz@gmail.com>
2019-02-12 01:17:57 -0500

    initial tests

--- spec/todoable_spec.rb
+++ spec/todoable_spec.rb
@@ -1,9 +1,30 @@
 RSpec.describe Todoable do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Todoable::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'when creating a new session' do
+    before(:all) do
+      @session = Todoable::Session.new('test', 'password')
+    end
+
+    it 'creates a Todoable::Session' do
+      expect(@session).to be_an_instance_of(Todoable::Session)
+    end
+
+    it 'authenticates the user' do
+      expect_any_instance_of(Todoable::Session).to receive(:authenticate)
+      Todoable::Session.new('test', 'test')
+    end
+
+    describe '.authenticate' do
+      xit 'gets the authentication token' do
+      end
+
+      it 'returns a token' do
+        token = @session.authenticate
+        expect(token).to eq({ 'token': 'fake-token' })
+      end
+    end
   end
 end
