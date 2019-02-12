ff02dc00cd2c504ed485dbef96d1b2cbd8a51da7
Andy Yong <andyywz@gmail.com>
2019-02-12 04:06:35 -0500

    update use cases and add more logic

--- lib/todoable.rb
+++ lib/todoable.rb
@@ -1,20 +1,27 @@
 require 'rest-client'
 require 'json'
 require 'todoable/version'
-require 'todoable/list'
+require 'todoable/list_helper'
+require 'todoable/item_helper'
 
 module Todoable
 
   class Error < StandardError; end
+  class UnauthorizedError < StandardError; end
 
   class Session
+    include Todoable::ListHelper
+    include Todoable::ItemHelper
+
     TOKEN_LIFESPAN = 20 * 60
-    AUTHENTICATION_PATH = 'http://todoable.teachable.tech/api/authenticate'
+    AUTH_URL = 'http://todoable.teachable.tech/api/authenticate'
 
     def initialize(username, password)
       @username = username
       @password = password
-      @token = authenticate
+      @token = nil
+
+      authenticate
     end
 
     def token_expired?
@@ -22,32 +29,60 @@ module Todoable
     end
 
     def authenticate
-      # make auth call here
-      token = RestClient.post(
-        AUTHENTICATION_PATH,
-        { username: @username, password: @password }.to_json,
-        { content_type: :json, accept: :json }
-      )
+      return @token if @token && !token_expired?
 
-      @token_expiry = Time.now + TOKEN_LIFESPAN
-      { 'token': 'fake-token' }
+      begin
+        payload = { username: @username, password: @password }.to_json
+        headers = { content_type: :json, accept: :json }
+
+        response = RestClient.post(AUTH_URL, payload, headers)
+
+        @token_expiry = Time.now + TOKEN_LIFESPAN
+        @token = JSON.parse(response.body)['token']
+      rescue RestClient::Unauthorized
+        # Log Error
+        puts 'the username and password combination does not match our records'
+        @token = nil
+      end
+
+      @token
     end
 
-    def make_call(url, params)
-      # make api call here
+    def invoke(method, url, payload = {})
+      authenticate
 
+      begin
+        response = RestClient::Request.execute(
+          method: method,
+          url: url,
+          payload: payload,
+          headers: {
+            Authorization: @token,
+            content_type: :json,
+            accept: :json
+          }
+        )
+
+        result = JSON.parse(response.body)
+      rescue RestClient::UnprocessableEntity
+        # Log Error
+        # TODO: Error handling
+        result = nil # return a proper error message
+      end
+
+      result
     end
   end
 
   # session = Todoable::Session.new(username, password)
-  # session.authenticate if session.token_expired?
 
-  # Todoable::List.get_all
-  # Todoable::List.create(list: { name: 'list name' })
-  # Todoable::List.get(:id)
-  # Todoable::List.update(:id, { list: { name: 'new name' } })
-  # Todoable::List.delete(:id)
+  # session.get_all
+  # session.create_list(list: { name: 'list name' })
+  # session.get(:id)
+  # session.update(:id, { list: { name: 'new name' } })
+  # session.delete_list(:id)
 
-  # Todoable::Item.complete(:list_id, :item_id)
-  # Todoable::Item.delete(:list_id, :id)
+  # session.create_item(:list_id, payload)
+  # session.complete(:list_id, :item_id)
+  # session.delete_item(:list_id, :id)
 end
