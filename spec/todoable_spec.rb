425736f1253cf0bf99c7766cba611d4e58f20e4a
Andy Yong <andyywz@gmail.com>
2019-02-12 04:05:59 -0500

    more specs

--- spec/todoable_spec.rb
+++ spec/todoable_spec.rb
@@ -1,29 +1,67 @@
 RSpec.describe Todoable do
-  it 'has a version number' do
-    expect(Todoable::VERSION).not_to be nil
-  end
-
-  describe 'when creating a new session' do
-    before(:all) do
-      @session = Todoable::Session.new('test', 'password')
-    end
+  describe Todoable::Session do
+    let (:username) { 'test' }
+    let (:password) { 'password' }
+    let (:url) { 'http://todoable.teachable.tech/api/authenticate' }
+    let (:payload) { { username: username, password: password }.to_json }
+    let (:headers) { { content_type: :json, accept: :json } }
 
     it 'creates a Todoable::Session' do
-      expect(@session).to be_an_instance_of(Todoable::Session)
+      allow(RestClient).to receive(:post)
+        .with(url, payload, headers)
+        .and_return(OpenStruct.new(body: { token: 'fake-token' }.to_json))
+
+      session = Todoable::Session.new(username, password)
+      expect(session).to be_an_instance_of(Todoable::Session)
     end
 
     it 'authenticates the user' do
       expect_any_instance_of(Todoable::Session).to receive(:authenticate)
-      Todoable::Session.new('test', 'test')
+
+      Todoable::Session.new(username, password)
     end
 
     describe '.authenticate' do
-      xit 'gets the authentication token' do
+      context 'when authentication succeeds' do
+        it 'returns a token' do
+          allow(RestClient).to receive(:post)
+            .with(url, payload, headers)
+            .and_return(OpenStruct.new(body: { token: 'fake-token' }.to_json))
+
+          session = Todoable::Session.new(username, password)
+          token = session.authenticate
+          expect(token).to eq('fake-token')
+        end
       end
 
-      it 'returns a token' do
-        token = @session.authenticate
-        expect(token).to eq({ 'token': 'fake-token' })
+      context 'when authentication fails' do
+        xit 'raises an error' do
+
+        end
+
+        xit 'returns nil' do
+
+        end
+      end
+    end
+
+    describe '.invoke' do
+      xit 'authenticates the token' do
+      end
+
+      xit 'executes a RestClient::Request' do
+      end
+
+      context 'when the request succeeds' do
+        xit 'returns a json response' do
+        end
+      end
+
+      context 'when the request fails' do
+        xit 'raises an error' do
+        end
+
+        xit 'returns nil'
       end
     end
   end
