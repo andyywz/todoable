8be4374b09d8b7560afc0de2089b7b5c0a6ef8de
Andy Yong <andyywz@gmail.com>
2019-02-15 00:19:36 -0500

    add url helper

--- lib/todoable/item_helper.rb
+++ lib/todoable/item_helper.rb
@@ -9,16 +9,17 @@ module Todoable
     end
 
     def mark_complete(list_id, item_id)
-      invoke(:put, "#{items_url(list_id)}/#{item_id}/finish")
+      invoke(:put, "#{items_url(list_id, item_id)}/finish")
     end
 
     def delete_item(list_id, item_id)
-      invoke(:delete, "#{items_url(list_id)}/#{item_id}")
+      invoke(:delete, items_url(list_id, item_id))
     end
 
   private
-    def items_url(list_id)
-      "#{BASE_URL}/#{list_id}/items"
+    def items_url(list_id, items_id = nil)
+      base_url = "#{BASE_URL}/#{list_id}/items"
+      items_id ? "#{base_url}/#{items_id}" : base_url
     end
   end
 end
--- lib/todoable/list_helper.rb
+++ lib/todoable/list_helper.rb
@@ -3,34 +3,38 @@ module Todoable
     BASE_URL = 'http://todoable.teachable.tech/api/lists'.freeze
 
     def lists
-      response = invoke(:get, BASE_URL)
+      response = invoke(:get, list_url)
 
       JSON.parse(response)['lists']
     end
 
     def get_list(id)
-      response = invoke(:get, "#{BASE_URL}/#{id}")
+      response = invoke(:get, list_url(id))
 
       JSON.parse(response)
     end
 
     def create_list(name)
-      response = invoke(:post, BASE_URL, payloadify(name))
+      response = invoke(:post, list_url, payloadify(name))
 
       JSON.parse(response)
     end
 
     def update_list_name(id, name)
-      invoke(:patch, "#{BASE_URL}/#{id}", payloadify(name))
+      invoke(:patch, list_url(id), payloadify(name))
     end
 
     def delete_list(id)
-      invoke(:delete, "#{BASE_URL}/#{id}")
+      invoke(:delete, list_url(id))
     end
 
   private
     def payloadify(name)
       { list: { name: name } }
+    end
+
+    def list_url(id = nil)
+      id ? "#{BASE_URL}/#{id}" : BASE_URL
     end
   end
 end
